37a5b7f6d93307f47cd2eb9947715a1b79436007
Foster Amponsah <foster.asante@gwosevo.com>
2025-04-17 10:16:25 +0000

    Project start-up

---
+++ src/main/Car.kt
@@ -0,0 +1,11 @@
+package main
+
+class Car {
+    var carName = "Toyota";
+    var carColor = "Yellow";
+
+    fun getCarDetails(): String
+    {
+        return "Name: ${this.carName}\nColor: ${this.carColor}";
+    }
+}
---
+++ src/main/hello.kt
@@ -0,0 +1,9 @@
+import main.Car
+
+//package main
+
+fun main() {
+    val newCar = Car();
+    val details = newCar.getCarDetails();
+    println(details);
+}
